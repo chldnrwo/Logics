e9b90e28f080051dfe80370fe7f72df8f8b5ec1c
chldnrwo <chldnrwo777@naver.com>
2023-02-27 00:23:52 +0900

    easy

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 class Main {
@@ -5,7 +9,58 @@ class Main {
 		Scanner in = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
 		
+		int H = in.nextInt();
+		int T = in.nextInt();
+		int C = in.nextInt();
+		int K = in.nextInt();
+		int G = in.nextInt();
 		
+		int t = in.nextInt();
 		
+		Map<String, Integer> map = new HashMap<>();
+		
+		map.put("H", H);
+		map.put("T", T);
+		map.put("C", C);
+		map.put("K", K);
+		map.put("G", G);
+		
+		for(int i=0;i<t;i++) {
+			int h1 = in.nextInt();
+			int t1 = in.nextInt();
+			int c1 = in.nextInt();
+			int k1 = in.nextInt();
+			int g1 = in.nextInt();
+			
+			int h0 = map.get("H");
+			int t0 = map.get("T");
+			int c0 = map.get("C");
+			int k0 = map.get("K");
+			int g0 = map.get("G");
+			h0-=h1;
+			t0-=t1;
+			c0-=c1;
+			k0-=k1;
+			g0-=g1;
+			
+			map.put("H", h0);
+			map.put("T", t0);
+			map.put("C", c0);
+			map.put("K", k0);
+			map.put("G", g0);
+			
+			int sum = h0+t0+c0+k0+g0;
+			List<String> list = new ArrayList<>(map.keySet());
+			
+//			list.sort( (o1,o2)->{
+//				if(map.get(o2)==map.get(o1)) {
+//					
+//				}else {
+//					map.get(o2).compareTo(map.get(o1));
+//				}
+//			});
+			
+		}
+		System.out.println(map);		
 	}
 }
---
+++ src/easy.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class easy {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		String str = in.next();
+		char[] arr = new char[str.length()];
+		
+		for(int i=0;i<str.length();i++) {
+			int a = str.charAt(i);
+			if(65<=a && a<=90) {
+				a+=32;
+			}else {
+				a-=32;
+			}
+			arr[i] = (char)a;
+		}
+		
+		for(int i=0;i<str.length();i++) {
+			System.out.print(arr[i]);
+		}
+	}
+}
