820bc0f8fc44fb220f7495ee7e4f2686f1a631d9
chldnrwo <chldnrwo777@naver.com>
2023-03-03 23:22:43 +0900

    백트하나 풀다맘

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 class Main{
+	static int[][] arr;
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
 		int N = in.nextInt();
-		int F = in.nextInt();
-		
-		N = (N / 100) * 100;
-		
-		for(int i=0;i<100;i++) {
-			if((N+i)%F==0) {
-				String s;
-				if(i<10) {
-					s = 0+Integer.toString(i);
-				}else {
-					s = Integer.toString(i);
-				}
-				
-				System.out.println(s);
-				break;
+		int M = in.nextInt();
+		arr = new int[N][N];
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				arr[i][j] = in.nextInt();
 			}
+		}
+		
+		
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
 		}
 	}
 }
