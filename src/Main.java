27f8820ac14e3517bc5256ace6c183211c546243
chldnrwo <chldnrwo777@naver.com>
2023-03-06 16:19:57 +0900

    3

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,47 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main{
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
 		
-		int N=in.nextInt();
+		int N = in.nextInt();
+		int M = in.nextInt();
 		
-		int[] arr = new int[N];
+		int[][] arr = new int[N][M];
 		
 		for(int i=0;i<N;i++) {
-			arr[i]=in.nextInt();
+			String s = in.next();
+			for(int j=0;j<M;j++) {
+				arr[i][j] = s.charAt(j);
+			}
 		}
 		
-		int[] arrB = arr.clone();
-		Arrays.sort(arrB);
-		//System.out.println(Arrays.toString(arr));
-		//System.out.println(Arrays.toString(arrB));
+		int MaxL = Math.min(N, M)-1;
 		
-		Integer[] arrR = new Integer[N];
-		for(int i=0;i<N;i++){
-			int a = arrB[i];
-			for(int j=0;j<N;j++) {
-				if(a==arr[j]) {
-					if(arrR[j]==null) {
-						arrR[j]=i;	
-						break;
+		int res=0;
+		while(true) {
+			
+			for(int i=0;i<N-(MaxL);i++) {
+				for(int j=0;j<M-(MaxL);j++) {
+					if(arr[i][j]==arr[i][j+MaxL]
+						&&arr[i][j]==arr[i+MaxL][j]
+						&&arr[i][j]==arr[i+MaxL][j+MaxL]) {
+						res = (MaxL+1) * (MaxL+1);
+						System.out.println(res);
+						System.exit(0);
 					}
 				}
 			}
+			MaxL--;
 		}
-		for(int i=0;i<N;i++) {
-			System.out.print(arrR[i]+" ");
-		}
+		
+//		for(int i=0;i<N;i++) {
+//			for(int j=0;j<M;j++) {
+//				System.out.print(arr[i][j]-48);
+//				
+//			}
+//			System.out.println();
 		
 	}
 }
