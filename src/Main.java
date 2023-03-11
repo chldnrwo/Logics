ff8289f93a08a6869bceb732ade7c17025a4d70d
chldnrwo <chldnrwo777@naver.com>
2023-03-11 15:35:35 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,48 @@
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 class Main{
-	static boolean[] visit;	
-	static int N;
-	static List<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		N = in.nextInt();
 		
-		for(int i=0;i<=N;i++) {
-			list.add(new ArrayList<>());
+		int L = in.nextInt();
+		int[] arr = new int[L];
+		for(int i=0;i<L;i++) {
+			arr[i] = in.nextInt();
 		}
-
-		for(int i=0;i<N;i++){
-		String s  = in.next();
-			for(int j=0;j<N;j++){
-				char a = s.charAt(j);
-				if(a == 'Y') {
-					list.get(i+1).add(j+1);
+		//Arrays.sort(arr);
+		int n = in.nextInt();
+		int a = 0;
+		int b = 1001;
+		
+		for(int i=0;i<L-1;i++) {
+			if(arr[i] < n) {
+				if( arr[i] > a) {
+					a = arr[i];
 				}
+			}else if(arr[i] > n) {
+				if(arr[i] < b) {
+					b = arr[i];
+				}
+			}else {	
+				System.out.println(0);
+				return;
 			}
 		}
-		int max=0;
-		for(int i=1;i<=N;i++) {
-			int z = bfs(i);
-			if(z>max) {
-				max = z;
-			}
-		}
-		System.out.println(max);
 		
-	}
-	public static int bfs(int n){
-		
-		visit = new boolean[N+1];
-		Deque<Integer> q = new LinkedList<>();
-		
-		visit[n] = true;
 		int cnt=0;
-		int[] lim = new int[N+1];
-		q.offer(n);
-		
-		while(!q.isEmpty()) {
-			int a = q.poll();
-			if(lim[a]==2) {
-				return cnt;
-			}
-			
-			for(int i=0;i<list.get(a).size();i++) {
-				int nex = list.get(a).get(i);
-				
-				if(visit[nex]) {
-					continue;
-				}
-				visit[nex] = true;
-				lim[nex] = lim[a]+1;
-				cnt++;
-				q.offer(nex);
+		for(int i=a+1;i<=n;i++) {
+			for(int j=n;j<=b-1;j++) {
+				if(i!=j) {
+					cnt++;					
+				}	
 			}
 		}
 		
+		System.out.println(cnt);
 		
-		return cnt;
 	}
+	
 }
-
-//a와 직접 친구거나
-//c친구가 중계해주거나
-//최대는 N일수 밖에 없음
-//완전탐색 써야되고, N이 50보다 작아서 시간복잡도도 버틸 수 있음
-//두단계만
