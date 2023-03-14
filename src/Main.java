64682fe3f60c99dacbadbc36859e63059a59607d
chldnrwo <chldnrwo777@naver.com>
2023-03-14 21:56:23 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,36 @@
 import java.util.Scanner;
 
 class Main{
-	static String king,stone;
+	static long x,y,r;
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		king = in.next();
-		stone = in.next();
-		int t = in.nextInt();
-
-		for(int i=0;i<t;i++) {
-			String m = in.next();
-			
-			move(m);
-		}
+		x = in.nextInt();
+		y = in.nextInt();
 		
-		System.out.println(king);
-		System.out.println(stone);
+		r = y*100/x;  //88
+		if(r>=99) {
+			System.out.println(-1);
+			return;
+		}
+		bi(0, 1000000000);
+		
 	}
-	public static void move(String s) {
-		char xk = king.charAt(0);
-		char xs = stone.charAt(0);
+	public static void bi(int bot, int top) {
+		int mid = (bot+top)/2;
+		int z = (int) ((y+mid)*100/(x+mid));
 		
-		int yk = king.charAt(1)-48;
-		int ys = stone.charAt(1)-48;
-		
-		if(s.equals("R")) {
-			xk++;
-		}else if(s.equals("L")) {
-			xk--;
-		}else if(s.equals("B")) {
-			yk--;
-		}else if(s.equals("T")) {
-			yk++;
-		}else if(s.equals("RT")) {
-			xk++;
-			yk++;
-		}else if(s.equals("LT")) {
-			xk--;
-			yk++;
-		}else if(s.equals("RB")) {
-			xk++;
-			yk--;
-		}else if(s.equals("LB")) {
-			xk--;
-			yk--;
+		if(bot>top) {
+			System.out.println(mid+1);
+			return;
 		}
 		
-		//System.out.println(xk+" "+yk+" "+xs+" "+ys);
-		
-		if(xk==xs && yk==ys) {
-			if(s.equals("R")) {
-				xs++;
-			}else if(s.equals("L")) {
-				xs--;
-			}else if(s.equals("B")) {
-				ys--;
-			}else if(s.equals("T")) {
-				ys++;
-			}else if(s.equals("RT")) {
-				xs++;
-				ys++;
-			}else if(s.equals("LT")) {
-				xs--;
-				ys++;
-			}else if(s.equals("RB")) {
-				xs++;
-				ys--;
-			}else if(s.equals("LB")) {
-				xs--;
-				ys--;
-			}
+		if(r >= z) {
+			bi(mid+1, top);
+		}else {
+			bi(bot, mid-1);
 		}
-		
-		//System.out.println(xk+" "+yk+" "+xs+" "+ys);
-		
-		if('A'<=xk && xk<= 'H' &&
-			1<=yk && yk<=8 &&
-			'A'<=xs && xs<= 'H' &&
-			1<=ys && ys<=8
-				) {
-			//System.out.println(xk+" "+yk+" "+xs+" "+ys);
-			king = String.valueOf(xk)+Integer.toString(yk);
-			stone = String.valueOf(xs)+Integer.toString(ys);
-		}
-		
-		
 	}
 }
-
-//킹이 돌이 잇는곳을 가면 같은 방향으로 돌 이동
+//시간초과 시뮬레이션 말고 수학으로 풀어야됨
+//이분탐색문제였음
