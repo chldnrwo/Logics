b84f4385dd7c9915f2318641e997efda56473768
chldnrwo <chldnrwo777@naver.com>
2023-03-21 11:35:48 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,117 +1,59 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		String a = in.next();
-		String b = in.next();
-		String c = in.next();
+		int n = in.nextInt();
+		int m = in.nextInt();
 		
-		int A = 0;
-		int B = 0;
-		int C = 0;
+		int[][] A = new int[n][m];
+		int[][] B = new int[n][m];
 		
-		switch(a) {
-		case "black":
-			A = 0;
-			break;
-		case "brown":
-			A = 1;
-			break;
-		case "red":
-			A = 2;
-			break;
-		case "orange":
-			A = 3;
-			break;
-		case "yellow":
-			A = 4;
-			break;
-		case "green":
-			A = 5;
-			break;
-		case "blue":
-			A = 6;
-			break;
-		case "violet":
-			A = 7;
-			break;
-		case "grey":
-			A = 8;
-			break;
-		case "white":
-			A = 9;
-			break;
+		for(int i=0;i<n;i++) {
+			String s = in.next();
+			for(int j=0;j<m;j++) {
+				A[i][j] = s.charAt(j)-48;
+			}
+		}
+
+		for(int i=0;i<n;i++) {
+			String s = in.next();
+			for(int j=0;j<m;j++) {
+				B[i][j] = s.charAt(j)-48;
+			}
 		}
 		
-		switch(b) {
-		case "black":
-			B = 0;
-			break;
-		case "brown":
-			B = 1;
-			break;
-		case "red":
-			B = 2;
-			break;
-		case "orange":
-			B = 3;
-			break;
-		case "yellow":
-			B = 4;
-			break;
-		case "green":
-			B = 5;
-			break;
-		case "blue":
-			B = 6;
-			break;
-		case "violet":
-			B = 7;
-			break;
-		case "grey":
-			B = 8;
-			break;
-		case "white":
-			B = 9;
-			break;
+		int cnt = 0;
+		for(int i=0;i<n-2;i++) {
+			for(int j=0;j<m-2;j++) {
+				//System.out.println(A[i][j]+" "+B[i][j]);
+				if(A[i][j] != B[i][j]) {
+					
+					cnt++;
+					for(int k=i;k<i+3;k++) {
+						for(int l=j;l<j+3;l++) {
+							if(A[k][l]==0) {
+								A[k][l]=1;
+							}else {
+								A[k][l]=0;
+							}
+						}
+					}
+					
+				}
+			}
 		}
 		
-		switch(c) {
-		case "black":
-			C = 1;
-			break;
-		case "brown":
-			C = 10;
-			break;
-		case "red":
-			C = 100;
-			break;
-		case "orange":
-			C = 1000;
-			break;
-		case "yellow":
-			C = 10000;
-			break;
-		case "green":
-			C = 100000;
-			break;
-		case "blue":
-			C = 1000000;
-			break;
-		case "violet":
-			C = 10000000;
-			break;
-		case "grey":
-			C = 100000000;
-			break;
-		case "white":
-			C = 1_000_000_000;
-			break;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(A[i][j] != B[i][j]) {
+					System.out.println(-1);
+					return;
+				}
+			}
 		}
-		
-		long result = (long)(A*10+B)*C;
-		System.out.println(result);
+		System.out.println(cnt);
 	}
 }
