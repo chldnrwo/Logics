5ad34c73bcede5db1ef4fae78a0cd56593b2aacc
chldnrwo <chldnrwo777@naver.com>
2023-03-24 21:54:22 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,9 @@ class Main{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int n = in.nextInt();
-		int cnt = 0;
-		while(n>0) {
-			cnt+=n%2;
-			n/=2;
-		}
-		System.out.println(cnt);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A*B);
 	}
 }
