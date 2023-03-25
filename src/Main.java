8885b13c5f43d66010b6b573d079321312b78817
chldnrwo <chldnrwo777@naver.com>
2023-03-25 10:34:44 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,33 @@ class Main{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int A = in.nextInt();
-		int B = in.nextInt();
+		int t = in.nextInt();
 		
-		System.out.println(A*B);
+		int maxX = Integer.MIN_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		int minX = Integer.MAX_VALUE;
+		int minY = Integer.MAX_VALUE;
+		
+		for(int i=0;i<t;i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			
+			if(maxX <= a) {
+				maxX = a;
+			}
+			if(maxY <= b) {
+				maxY = b;
+			}
+			
+			if(minX >= a) {
+				minX = a;
+			}
+			if(minY >= b) {
+				minY = b;
+			}
+		}
+		
+		int result = (maxX - minX) * (maxY - minY);
+		System.out.println(result);
 	}
 }
