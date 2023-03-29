bb2fb9f2e793bb5d397aec51b8ecf084bff65634
chldnrwo <chldnrwo777@naver.com>
2023-03-29 20:18:04 +0900

    jj

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,26 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main{
-	static int[] arr;
-	static int[] arrS = new int[6];
-	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 		
-		while(true) {
-			int a = in.nextInt();
-			if(a==0) {
-				break;
-			}
-			
-			arr = new int[a];
-			for(int i=0;i<a;i++) {
-				arr[i] = in.nextInt();
-			}
-			
-			bt(0, 0);
-			sb.append("\n");
+		int n = in.nextInt();
+		int a = 0;
+		int cnt = 2;
+		while(a != n) {
+			cnt = cnt+cnt-1;
+			a++;
 		}
-		System.out.println(sb);
-	}
-	public static void bt(int depth, int start) {
-		if(depth==6) {
-			for(int i=0;i<6;i++) {
-				sb.append(arrS[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=start;i<arr.length;i++) {
-			arrS[depth] = arr[i];
-			bt(depth+1, i+1);
-		}
+		System.out.println(cnt*cnt);
 	}
 }
+/*
+ * 2
+ * 3
+ * 5
+ * 9
+ * 17
+ * 33
+ */
