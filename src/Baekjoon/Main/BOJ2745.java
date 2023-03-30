99aaef45aa716e8905906fa8d9619172e58d3c9e
chldnrwo <chldnrwo777@naver.com>
2023-03-30 17:22:54 +0900

    st

---
+++ src/Baekjoon/Main/BOJ2745.java
@@ -0,0 +1,26 @@
+package Baekjoon.Main;
+import java.util.Scanner;
+
+class BOJ2745{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String N = in.next();
+		int B = in.nextInt();
+		int len = N.length();
+		
+		int sum=0;
+		for(int i=len-1;i>=0;i--) {
+			char a = N.charAt(i);
+			int num;
+			if(a-57>0) {
+				num = a - 55; 
+			}else {
+				num = a - 48;
+			}
+			int z = (int) Math.pow(B, len-1-i);
+			sum += num*z;
+		}
+		System.out.println(sum);
+	}
+}
