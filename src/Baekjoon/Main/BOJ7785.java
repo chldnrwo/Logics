b9792fe80d517815231a5e52f43620441112256b
chldnrwo <chldnrwo777@naver.com>
2023-03-31 17:07:17 +0900

    second

--- src/Baekjoon/Main/BOJ7785.java
+++ src/Baekjoon/Main/BOJ7785.java
@@ -1,3 +1,4 @@
+package Baekjoon.Main;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -6,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-class Main{
+class BOJ7785{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
