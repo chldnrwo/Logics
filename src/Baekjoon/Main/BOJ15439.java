4f6b0f6dda62c54228100c7c4e075fb625af7b5e
chldnrwo <chldnrwo777@naver.com>
2023-04-01 12:45:24 +0900

    ss

---
+++ src/Baekjoon/Main/BOJ15439.java
@@ -0,0 +1,12 @@
+package Baekjoon.Main;
+import java.util.Scanner;
+
+class BOJ15439{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		System.out.println(N*(N-1));
+	}
+}
