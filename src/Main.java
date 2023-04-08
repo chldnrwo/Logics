c8efbeb4c942cae724344b8a20cc3e8ff42ea3a1
chldnrwo <chldnrwo777@naver.com>
2023-04-08 23:36:20 +0900

    m

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 class Main{
-	public static void main(String[] args) throws IOException {
-		//Scanner in = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+	static StringBuilder sb;
+	static boolean[] arr;
+	static int a;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		sb = new StringBuilder();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		Map<String, Integer> map = new HashMap<>();
-		for(int i=0;i<N;i++) {
-			String s = br.readLine();
+		while(in.hasNext()) {
+			a = in.nextInt();
+			int a3 = (int) Math.pow(3, a);
+			arr = new boolean[a3];
+			d(0, a3-1, 0);
 			
-			if(s.length()<M) {
-				continue;
-			}
-			
-			if(map.containsKey(s)) {
-				Integer cnt = map.get(s);
-				map.put(s, cnt+1);
-			}else {
-				map.put(s, 0);
-			}
-		}
-		
-		List<String> list = map.keySet().stream().collect(Collectors.toList());
-		
-		Collections.sort(list, (s1, s2)->{
-			int o1 = map.get(s1);
-			int o2 = map.get(s2);
-			
-			if(o1==o2) {
-				if(s1.length() == s2.length()) {
-					return s1.compareTo(s2);
+			for(boolean check : arr) {
+				if(!check) {
+					System.out.print("-");
+				}else {
+					System.out.print(" ");
 				}
-				return s2.length() - s1.length(); 
 			}
-			
-			return o2 - o1;
-		});
-		
-		for(String s : list) {
-			sb.append(s).append("\n");
+			System.out.println();
 		}
-		System.out.println(sb);
+		
 	}
-	
+	public static void d(int start, int end, int cnt) {
+		if(cnt==a) {
+			return;
+		}
+		
+		int ch1 = start + (end-start+1) / 3 - 1;
+		int ch2 = start + (end-start+1) * 2 / 3 - 1;
+		
+		
+		
+		for(int i=ch1+1;i<=ch2;i++) {
+			arr[i] = true;
+		}
+		
+		d(start, ch1, cnt+1);
+		d(ch2+1 ,end, cnt+1);
+	}
 }
+//0 8
