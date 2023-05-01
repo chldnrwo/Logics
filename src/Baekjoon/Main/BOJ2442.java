a6f37ab4066b412efaa79f62e380d1305d1d53d5
chldnrwo <chldnrwo777@naver.com>
2023-05-01 22:40:00 +0900

    dd

--- src/Baekjoon/Main/BOJ2442.java
+++ src/Baekjoon/Main/BOJ2442.java
@@ -1,3 +1,4 @@
+package Baekjoon.Main;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -8,7 +9,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
-class Main{
+class BOJ2442{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int r = in.nextInt();
