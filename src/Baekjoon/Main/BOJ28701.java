3f226e3ee954d550934006438100158a24a663d5
chldnrwo <chldnrwo777@naver.com>
2023-08-14 22:33:20 +0900

    dd

--- src/Baekjoon/Main/BOJ28701.java
+++ src/Baekjoon/Main/BOJ28701.java
@@ -1,3 +1,4 @@
+package Baekjoon.Main;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.Stack;
 
-public class Main{
+public class BOJ28701{
 	public static void main(String[] args) throws Exception{
 		Scanner in = new Scanner(System.in);
 		
