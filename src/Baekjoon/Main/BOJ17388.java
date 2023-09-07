c9f8e9c725e5d45591b2b2233ab21f87c2965d0c
chldnrwo <chldnrwo777@naver.com>
2023-09-07 23:39:56 +0900

    aa

--- src/Baekjoon/Main/BOJ17388.java
+++ src/Baekjoon/Main/BOJ17388.java
@@ -1,6 +1,7 @@
+package Baekjoon.Main;
 import java.util.*;
 
-public class Main{
+public class BOJ17388{
 
 	public static void main(String[] args) throws Exception{
 		Scanner in = new Scanner(System.in);
