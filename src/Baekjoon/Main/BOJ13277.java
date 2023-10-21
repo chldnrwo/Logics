206a60908acb65e5e3f83a96ca40858a36adbd76
chldnrwo <chldnrwo777@naver.com>
2023-10-21 20:10:06 +0900

    aa

--- src/Baekjoon/Main/BOJ13277.java
+++ src/Baekjoon/Main/BOJ13277.java
@@ -1,7 +1,8 @@
+package Baekjoon.Main;
 import java.math.BigInteger;
 import java.util.*;
 
-public class Main{
+public class BOJ13277{
 	static Integer[] arr;
 	public static void main(String[] args) throws Exception{
 		Scanner in = new Scanner(System.in);
