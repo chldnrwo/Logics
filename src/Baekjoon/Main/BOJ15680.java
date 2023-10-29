aeb06147ddae8f7a0c7aa316d178a06faf137904
chldnrwo <chldnrwo777@naver.com>
2023-10-29 23:33:13 +0900

    aa

---
+++ src/Baekjoon/Main/BOJ15680.java
@@ -0,0 +1,20 @@
+package Baekjoon.Main;
+import java.math.BigInteger;
+import java.util.*;
+
+public class BOJ15680{
+	static Integer[] arr;
+	public static void main(String[] args) throws Exception{
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		
+		if(n==0) {
+			System.out.println("YONSEI");
+		}else if(n==1) {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+	
+}
+
