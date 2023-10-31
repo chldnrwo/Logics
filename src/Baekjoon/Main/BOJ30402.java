b5e6de2537509d3008dd50d4595eea9e6f590c3d
chldnrwo <chldnrwo777@naver.com>
2023-10-31 23:44:28 +0900

    aa

--- src/Baekjoon/Main/BOJ30402.java
+++ src/Baekjoon/Main/BOJ30402.java
@@ -2,23 +2,33 @@ package Baekjoon.Main;
 
 import java.util.*;
 
-public class Main{
-	static Integer[] arr;
+public class BOJ30402{
 	public static void main(String[] args) throws Exception{
 		Scanner in = new Scanner(System.in);
 		
+		char[][] arr = new char[15][15];
+		
 		for(int i=0;i<15;i++) {
 			String s = in.nextLine();
-			System.out.println(s);
 			for(int j=0;j<s.length();j++) {
-				if(s.charAt(j) == 'w') {
+				if(s.charAt(j) != ' ') {
+					//System.out.print(s.charAt(j));
+					arr[i][(j+1)/2] = s.charAt(j); 
+				}
+			}
+		}
+		
+		
+		for(int i=0;i<15;i++) {
+			for(int j=0;j<15;j++) {
+				if(arr[i][j] == 'w') {
 					System.out.println("chunbae");
 					System.exit(0);
-				}else if(s.charAt(j) == 'b') {
+				}else if(arr[i][j] == 'b') {
 					System.out.println("nabi");
 					System.exit(0);
-				}else if(s.charAt(j) == 'g') {
-					System.out.println("yeungcheol");
+				}else if(arr[i][j] == 'g') {
+					System.out.println("yeongcheol");
 					System.exit(0);
 				}
 			}
