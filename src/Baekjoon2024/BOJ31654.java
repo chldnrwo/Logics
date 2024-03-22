ac57351abeee0ae6b0fda88459f9ce6f403fb09c
Choi WukJae <chldnrwo777@naver.com>
2024-03-22 15:44:16 +0900

    aa

--- src/Baekjoon2024/BOJ31654.java
+++ src/Baekjoon2024/BOJ31654.java
@@ -1,9 +1,10 @@
+package Baekjoon2024;
 import java.util.*;
 import java.io.*;
  
 
  
-class Main {
+class BOJ31654 {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         
