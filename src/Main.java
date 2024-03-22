5b2c7e0ecdef45c25f536f2a301e066f6756152b
Choi WukJae <chldnrwo777@naver.com>
2024-03-22 18:05:08 +0900

    하던거

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,56 @@ import java.io.*;
 
  
 class Main {
-	static int[] arr;
+	static int n, m, tar, get;
+	static List<ArrayList<Integer>> list;
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         
-        int n = in.nextInt();
-        arr = new int[1001];
-        arr[1] = 1;
-        arr[0] = 0;
-        arr[2] = 3;
+        n = in.nextInt();
+        tar = in.nextInt();
+        get = in.nextInt();
         
-        System.out.println(dp(n));
+        m =in.nextInt();
+        list = new ArrayList<ArrayList<Integer>>();
+        for(int i=0;i<=n;i++) {
+        	list.add(new ArrayList<Integer>());
+        }
+        
+        for(int i=0;i<m;i++) {
+        	int a = in.nextInt();
+        	int b = in.nextInt();
+        	
+        	list.get(a).add(b);
+        	list.get(b).add(a);
+        }
+        
+        
+        
     }
-    public static int dp(int num) {
-    	if(arr[num]==0) {
-    		arr[num] = (dp(num-2)*2 + dp(num-1))%10007;
+    public static int bfs() {
+    	int cnt = 0;
+    	Deque<Integer> dq = new LinkedList<Integer>();
+    	boolean[] visit = new boolean[n+1];
+    	visit[tar] = true;
+    	dq.offer(tar);
+    	
+    	while(!dq.isEmpty()) {
+    		int nex = dq.poll();
+    		
+    		for(int i=0;i<list.get(nex).size();i++) {
+    			int ne = list.get(nex).get(i);
+    			
+    			if(!visit[ne]) {
+    				visit[ne] = true;
+    				
+    				dq.offer(ne);
+    			}
+    		}
     	}
     	
-    	return arr[num];
+    	
+    	
+    	
+    	return 0;
     }
 }
