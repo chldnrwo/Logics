874adde6b013858837c69152af59e5b1dc324595
chldnrwo <chldnrwo777@naver.com>
2024-03-23 09:42:19 +0900

    풀던거

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,69 @@
 import java.util.*;
 import java.io.*;
+import java.math.BigInteger;
  
 
  
 class Main {
 	static StringBuilder sb;
-	static int n,m;
-	static int[] arr, res;
-	static boolean[] check;
+	static int n;
+	static BigInteger[] arr;
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
         sb = new StringBuilder();
-        n = in.nextInt();
-        m = in.nextInt();
+        arr = new BigInteger[101];
+        arr[1] = new BigInteger("1");
         
-        arr = new int[n];
-        res = new int[n];
-        check = new boolean[n];
-        for(int i=0;i<n;i++) {
-        	arr[i] = in.nextInt();
+        if(n>20) {
+        	System.out.println(dp(n));
         }
         
-        Arrays.sort(arr);
-        bt(0);
-        System.out.println(sb);
-        
     }
-    public static void bt(int num) {
-    	if(num == m) {
-    		for(int i=0;i<m;i++) {
-    			sb.append(res[i]).append(" ");
-    		}
-    		sb.append("\n");
-    		return;
+    public static BigInteger dp(int num) {
+    	if(arr[num] == null) {
+    		arr[num] = dp(num-1).multiply(new BigInteger("2")).add(new BigInteger("1"));
     	}
-    	
-    	int ch = 0;
-    	
-    	for(int i=0;i<n;i++) {
-    		if(!check[i] && arr[i] != ch) {
-    			res[num] = arr[i];
-    			ch = res[num];
-    			check[i] = true;
-    			bt(num + 1);
-    			check[i] = false;
-    		}
-    	}
-    	
-    	
-    }
+
+    	return arr[num];
+    } 
+    /*
+2    3
+1 2
+1 3
+2 1 
+
+
+3    7
+1 3
+1 2
+3 2
+1 3
+2 1
+2 3
+1 3
+
+4    15
+1 2
+1 3
+2 1
+1 2
+3 1
+3 2
+1 2
+1 3
+2 3
+2 1
+3 1
+2 3
+1 2
+1 3
+2 3
+
+     */
+    
+    
+    
     
     
 }
