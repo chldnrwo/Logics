580e33a4f7af5df8817f6ed8b63b99864423337f
chldnrwo <chldnrwo777@naver.com>
2024-04-17 00:57:04 +0900

    aa

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,12 @@ class Main {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         
-        int n = in.nextInt();
-        
-        int min = Integer.MAX_VALUE;
-        for(int i=0;i<n;i++) {
-        	int a = in.nextInt();
-        	int b = in.nextInt();
-        	
-        	if(a>b) {
-        		continue;
-        	}
-        	
-        	min = Math.min(b, min);
-        	
-        }
-        
-        
-        if(min == Integer.MAX_VALUE) {
-        	System.out.println(-1);
-        }else {
-        	System.out.println(min);
-        }
+        System.out.println("(___)\n"
+        		+ "(o o)____/\n"
+        		+ " @@      \\\n"
+        		+ "  \\ ____,/\n"
+        		+ "  //   //\n"
+        		+ " ^^   ^^");
     }
     
   
