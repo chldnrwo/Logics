54a4c7403d22812f4073f8e81a5aa9bc8f50ab66
chldnrwo <chldnrwo777@naver.com>
2024-04-19 23:52:53 +0900

    aa

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,11 @@ class Main {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         
-        System.out.println("(___)\n"
-        		+ "(o o)____/\n"
-        		+ " @@      \\\n"
-        		+ "  \\ ____,/\n"
-        		+ "  //   //\n"
-        		+ " ^^   ^^");
+        int n = in.nextInt();
+        
+        for(int i=0;i<n;i++) {
+        	System.out.println("SciComLove");
+        }
     }
     
   
