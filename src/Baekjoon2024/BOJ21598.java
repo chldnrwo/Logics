c677ab3a558848fead63754740c4196f37cb0fbc
chldnrwo <chldnrwo777@naver.com>
2024-04-19 23:53:13 +0900

    aa

---
+++ src/Baekjoon2024/BOJ21598.java
@@ -0,0 +1,22 @@
+package Baekjoon2024;
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+ 
+
+ 
+class BOJ21598 {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        
+        for(int i=0;i<n;i++) {
+        	System.out.println("SciComLove");
+        }
+    }
+    
+  
+  
+} 
+ 
