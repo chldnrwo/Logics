7c02a85a722ba8f757241ec95535a4bf23fc2a08
chldnrwo <chldnrwo777@naver.com>
2024-05-11 20:07:13 +0900

    aa

--- src/Baekjoon2024/BOJ15733.java
+++ src/Baekjoon2024/BOJ15733.java
@@ -1,10 +1,11 @@
+package Baekjoon2024;
 import java.util.*;
 import java.io.*;
 import java.math.BigInteger;
  
 
  
-class Main {
+class BOJ15733 {
     public static void main(String[] args) throws Exception {
        Scanner in = new Scanner(System.in);
        System.out.println("I'm Sexy");
