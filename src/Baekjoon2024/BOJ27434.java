620e42a5488ca94452e2987580ef3d2a681f90db
chldnrwo <chldnrwo777@naver.com>
2024-06-26 20:36:54 +0900

    aa

--- src/Baekjoon2024/BOJ27434.java
+++ src/Baekjoon2024/BOJ27434.java
@@ -1,9 +1,10 @@
+package Baekjoon2024;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main {
+public class BOJ27434 {
     public static void main(String[] args) throws Exception {
 
 
