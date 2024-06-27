8b1a357bad34b1ee602d405a3a7a7f7e2abaa7d3
chldnrwo <chldnrwo777@naver.com>
2024-06-27 23:46:25 +0900

    aa

---
+++ src/Baekjoon2024/BOJ4299.java
@@ -0,0 +1,21 @@
+package Baekjoon2024;
+import java.util.Scanner;
+
+public class BOJ4299 {
+    public static void main(String[] args) throws Exception {
+    	Scanner in = new Scanner(System.in);
+    	
+    	int a = in.nextInt();
+    	int b = in.nextInt();
+    	
+		if((a + b) % 2 != 0 || a < b) {
+			System.out.println(-1);
+		}else {
+			
+			System.out.println(((a + b) / 2) + " " + (((a + b) / 2) - b));
+		}
+    	
+    }
+
+   
+}
