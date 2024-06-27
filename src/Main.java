c3bcf6ca898bc7d1dfbfd0206b3f24467992ac8e
chldnrwo <chldnrwo777@naver.com>
2024-06-27 23:45:56 +0900

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.valueOf(br.readLine());
-
-        System.out.println(factorial(1, N));
+    	Scanner in = new Scanner(System.in);
+    	
+    	int a = in.nextInt();
+    	int b = in.nextInt();
+    	
+		if((a + b) % 2 != 0 || a < b) {
+			System.out.println(-1);
+		}else {
+			
+			System.out.println(((a + b) / 2) + " " + (((a + b) / 2) - b));
+		}
+    	
     }
 
-    public static BigInteger factorial(int a, int n){
-        BigInteger num = BigInteger.valueOf(a);
-
-        if (a < n){
-            int b = (a + n) / 2;
-            num = factorial(a,b).multiply(factorial(b+1, n));
-        }
-        return num;
-    }
+   
 }
